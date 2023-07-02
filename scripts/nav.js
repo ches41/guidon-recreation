475fc229e19ec1521157e9873e5977ff5cc6e03b
ches41 <fdjr401@gmail.com>
2023-07-02 19:45:13 +0800

    Add header and navigation bar

---
+++ scripts/nav.js
@@ -0,0 +1,8 @@
+// var for header height
+// var for current page position?
+
+function toggleNav() {
+    // check if person has scrolled past header
+    // if yes, show nav background and button
+    // if no, hide nav background and button
+}
